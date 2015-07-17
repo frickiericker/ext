723b5c07661b5eaf1a8991810f1192b927fd55e6
sinfu <nobody@example.com>
2015-07-17 11:47:09 +0900

    [TODO] random: random_engine_facade

--- include/ext/random.hpp
+++ include/ext/random.hpp
@@ -1,12 +1,171 @@
 #ifndef EXT_RANDOM_HPP
 #define EXT_RANDOM_HPP
 
+#include <algorithm>
+#include <array>
+#include <iterator>
 #include <limits>
 #include <random>
+#include <type_traits>
+#include <utility>
 
-#include <cstddef>
 #include <cmath>
+#include <cstddef>
+#include <cstdint>
 
+#include "meta.hpp"
+
+
+//____ Metafunction __________________________________________________________
+
+namespace ext
+{
+    namespace detail
+    {
+        template<typename S>
+        auto try_seed_seq_generate(
+                S&             q  = std::declval<S&>(),
+                unsigned long* ib = std::declval<unsigned long*>(),
+                unsigned long* ie = std::declval<unsigned long*>()
+                )
+            -> decltype(q.generate(ib, ie));
+
+        template<typename T>
+        using seed_seq_generate_t = decltype(try_seed_seq_generate<T>());
+    }
+
+    /*
+     * Determines whether T satisfies the seed sequence requirements.
+     */
+    template<typename T>
+    struct is_seed_sequence : n4502::is_detected<detail::seed_seq_generate_t, T>
+    {
+    };
+}
+
+//____ Engine ________________________________________________________________
+
+namespace ext
+{
+    /*
+     * TODO
+     */
+    template<typename Derived, typename T>
+    struct random_engine_facade
+    {
+        using derived_type = Derived;
+        using result_type = T;
+
+        // ____ Constructors and seeding functions ___________________________
+
+        random_engine_facade()
+        {
+            derived().seed();
+        }
+
+        explicit
+        random_engine_facade(result_type s)
+        {
+            derived().seed(s);
+        }
+
+        template< typename SeedSeq
+                , std::enable_if_t<is_seed_sequence<SeedSeq>::value, int> = 0>
+        explicit
+        random_engine_facade(SeedSeq& s)
+        {
+            derived().seed(s);
+        }
+
+        void seed()
+        {
+            derived().seed(derived_type::default_seed);
+        }
+
+        void seed(result_type s)
+        {
+            std::seed_seq seq{s};
+            derived().seed(seq);
+        }
+
+        /* TODO
+        template< typename SeedSeq
+                , std::enable_if_t<is_seed_sequence<SeedSeq>::value, int> = 0>
+        void seed(SeedSeq& s)
+        {
+            using std::begin;
+            using std::end;
+        }
+        */
+
+        // ____ Random number generation _____________________________________
+
+        result_type operator()()
+        {
+            derived().advance();
+            return derived().value();
+        }
+
+        void discard(unsigned long long z)
+        {
+            derived().advance(z);
+        }
+
+        // ____ Engine state _________________________________________________
+
+        /* TODO
+        bool operator==(random_engine_facade const& other) const
+        {
+            return derived().state() == other.derived().state();
+        }
+
+        bool operator!=(random_engine_facade const& other) const
+        {
+            return !(*this == other);
+        }
+
+        template<typename Char, typename CharTraits>
+        friend
+        std::basic_ostream<Char, CharTraits>&
+        operator<<(std::basic_ostream<Char, CharTraits>& output_stream,
+                   random_engine_facade const& engine)
+        {
+            return output_stream;
+        }
+
+        template<typename Char, typename CharTraits>
+        friend
+        std::basic_istream<Char, CharTraits>&
+        operator>>(std::basic_istream<Char, CharTraits>& input_stream,
+                   random_engine_facade& engine)
+        {
+            return input_stream;
+        }
+        */
+
+        // ____ CRTP _________________________________________________________
+
+        void advance(unsigned long long z)
+        {
+            for (auto i = 0uLL; i < z; ++i)
+                derived().advance();
+        }
+
+      private:
+
+        derived_type& derived() noexcept
+        {
+            return static_cast<derived_type&>(*this);
+        }
+
+        derived_type const& derived() const noexcept
+        {
+            return static_cast<derived_type const&>(*this);
+        }
+    };
+}
+
+//____ Algorithm _____________________________________________________________
 
 namespace ext
 {
