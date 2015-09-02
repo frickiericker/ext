fa891bfc182c3536bba42efab39b9d43109fe90c
sinfu <nobody@example.com>
2015-09-02 17:13:37 +0900

    array_view: Add conversion constructor

--- include/ext/array_view.hpp
+++ include/ext/array_view.hpp
@@ -4,7 +4,6 @@
 #include <algorithm>
 #include <array>
 #include <iterator>
-#include <stdexcept>
 #include <type_traits>
 #include <utility>
 #include <vector>
@@ -15,8 +14,10 @@
 
 namespace ext
 {
+    constexpr struct assume_safe_array_tag {} assume_safe_array {};
+
     /**
-     * Non-owning view of a sequential memory region.
+     * Non-owning view of contiguous memory region.
      */
     template<typename T>
     struct array_view
@@ -34,7 +35,7 @@ namespace ext
         using size_type = std::size_t;
         using difference_type = std::ptrdiff_t;
 
-        //__ Creation ______________________________________________________
+        // Creation ____________________________________________________________
 
         /**
          * Constructs a null view.
@@ -46,74 +47,32 @@ namespace ext
         }
 
         /**
-         * Constructs a view into the array holded by a std::vector.
-         */
-        template<typename Allocator>
-        explicit
-        array_view(std::vector<value_type, Allocator>& vec) noexcept
-            : ptr_(vec.data()), size_(vec.size())
-        {
-        }
-
-        template<typename Allocator>
-        explicit
-        array_view(std::vector<value_type, Allocator> const& vec) noexcept
-            : ptr_(vec.data()), size_(vec.size())
-        {
-        }
-
-        /**
-         * Constructs a view into a std::array.
-         */
-        template<size_type N>
-        explicit constexpr
-        array_view(std::array<value_type, N>& ary) noexcept
-            : ptr_(ary.data()), size_(ary.size())
-        {
-        }
-
-        template<size_type N>
-        explicit constexpr
-        array_view(std::array<value_type, N> const& ary) noexcept
-            : ptr_(ary.data()), size_(ary.size())
-        {
-        }
-
-        /**
-         * Constructs a view into a built-in array.
-         */
-        template<size_type N>
-        explicit constexpr
-        array_view(access_type(& ary)[N]) noexcept
-            : ptr_(ary), size_(N)
-        {
-        }
-
-        /**
-         * Constructs a view into a memory region.
+         * Constructs a view into the memory region.
          */
         constexpr
-        array_view(pointer ptr, size_type count)
+        array_view(pointer ptr, size_type size)
+            : ptr_(ptr), size_(size)
+        {
+        }
+
+        constexpr
+        array_view(pointer ptr, size_type count, assume_safe_array_tag) noexcept
             : ptr_(ptr), size_(count)
         {
         }
 
-        //__ Range Support _________________________________________________
+        /**
+         * Conversion.
+         */
+        template<typename U,
+                 std::enable_if_t<std::is_convertible<U*, T*>::value, int> = 0>
+        constexpr
+        array_view(array_view<U> other) noexcept
+            : array_view(other.data(), other.size(), assume_safe_array)
+        {
+        }
 
-        constexpr iterator begin() noexcept { return ptr_; }
-        constexpr iterator end() noexcept { return ptr_ + size_; }
-        constexpr const_iterator begin() const noexcept { return ptr_; }
-        constexpr const_iterator end() const noexcept { return ptr_ + size_; }
-        constexpr const_iterator cbegin() const noexcept { return ptr_; }
-        constexpr const_iterator cend() const noexcept { return ptr_ + size_; }
-        reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
-        reverse_iterator rend() noexcept { return reverse_iterator(end()); }
-        const_reverse_iterator rbegin() const noexcept { return reverse_iterator(end()); }
-        const_reverse_iterator rend() const noexcept { return reverse_iterator(end()); }
-        const_reverse_iterator crbegin() const noexcept { return reverse_iterator(begin()); }
-        const_reverse_iterator crend() const noexcept { return reverse_iterator(begin()); }
-
-        //__ Element Access Functions ______________________________________
+        // Element access functions ____________________________________________
 
         constexpr
         reference operator[](size_type idx)
@@ -124,22 +83,6 @@ namespace ext
         constexpr
         const_reference operator[](size_type idx) const
         {
-            return ptr_[idx];
-        }
-
-        constexpr
-        reference at(size_type idx)
-        {
-            if (idx > size_)
-                throw std::range_error("out of range");
-            return ptr_[idx];
-        }
-
-        constexpr
-        const_reference at(size_type idx) const
-        {
-            if (idx > size_)
-                throw std::range_error("out of range");
             return ptr_[idx];
         }
 
@@ -179,7 +122,7 @@ namespace ext
             return ptr_;
         }
 
-        //__ Capacity Query ________________________________________________
+        // Capacity query ______________________________________________________
 
         constexpr
         size_type size() const noexcept
@@ -193,7 +136,22 @@ namespace ext
             return size() == 0;
         }
 
-        //__ Range Modification ____________________________________________
+        // Range support _______________________________________________________
+
+        constexpr iterator begin() noexcept { return ptr_; }
+        constexpr iterator end() noexcept { return ptr_ + size_; }
+        constexpr const_iterator begin() const noexcept { return ptr_; }
+        constexpr const_iterator end() const noexcept { return ptr_ + size_; }
+        constexpr const_iterator cbegin() const noexcept { return ptr_; }
+        constexpr const_iterator cend() const noexcept { return ptr_ + size_; }
+        reverse_iterator rbegin() noexcept { return reverse_iterator(end()); }
+        reverse_iterator rend() noexcept { return reverse_iterator(end()); }
+        const_reverse_iterator rbegin() const noexcept { return reverse_iterator(end()); }
+        const_reverse_iterator rend() const noexcept { return reverse_iterator(end()); }
+        const_reverse_iterator crbegin() const noexcept { return reverse_iterator(begin()); }
+        const_reverse_iterator crend() const noexcept { return reverse_iterator(begin()); }
+
+        // Range modification __________________________________________________
 
         constexpr
         void pop_front()
@@ -234,11 +192,9 @@ namespace ext
             a.swap(b);
         }
 
-        //__________________________________________________________________
-
       private:
 
-        pointer ptr_;
+        pointer   ptr_;
         size_type size_;
     };
 
@@ -288,13 +244,13 @@ namespace ext
     template<typename T, typename Allocator>
     array_view<T> make_array_view(std::vector<T, Allocator>& vec) noexcept
     {
-        return array_view<T>(vec);
+        return array_view<T>(vec.data(), vec.size(), assume_safe_array);
     }
 
     template<typename T, typename Allocator>
     array_view<T const> make_array_view(std::vector<T, Allocator> const& vec) noexcept
     {
-        return array_view<T const>(vec);
+        return array_view<T const>(vec.data(), vec.size(), assume_safe_array);
     }
 
     /**
@@ -304,14 +260,14 @@ namespace ext
     constexpr
     array_view<T> make_array_view(std::array<T, N>& ary) noexcept
     {
-        return array_view<T>(ary);
+        return array_view<T>(ary.data(), ary.size(), assume_safe_array);
     }
 
     template<typename T, std::size_t N>
     constexpr
     array_view<T const> make_array_view(std::array<T, N> const& ary) noexcept
     {
-        return array_view<T const>(ary);
+        return array_view<T const>(ary.data(), ary.size(), assume_safe_array);
     }
 
     /**
@@ -321,32 +277,33 @@ namespace ext
     constexpr
     array_view<T> make_array_view(T(& ary)[N]) noexcept
     {
-        return array_view<T>(ary);
+        return array_view<T>(ary, N, assume_safe_array);
     }
 
     template<typename T, std::size_t N>
     constexpr
     array_view<T const> make_array_view(T const(& ary)[N]) noexcept
     {
-        return array_view<T const>(ary);
+        return array_view<T const>(ary, N, assume_safe_array);
     }
 
     /**
      * Creates an array_view into raw memory region. Behavior is undefined if
-     * the specified memory region is invalid (hence no noexcept).
+     * the specified memory region is invalid (hence this function is not
+     * noexcept).
      */
     template<typename T>
     constexpr
-    array_view<T> make_array_view(T* ptr, std::size_t count)
+    array_view<T> make_array_view(T* ptr, std::size_t size)
     {
-        return array_view<T>(ptr, count);
+        return array_view<T>(ptr, size);
     }
 
     template<typename T, std::size_t N>
     constexpr
-    array_view<T const> make_array_view(T const* ptr, std::size_t count)
+    array_view<T const> make_array_view(T const* ptr, std::size_t size)
     {
-        return array_view<T const>(ptr, count);
+        return array_view<T const>(ptr, size);
     }
 }
 
