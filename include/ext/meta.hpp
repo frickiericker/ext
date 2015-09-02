ea304833048b240e09ba462f54a565f2becd8be9
sinfu <nobody@example.com>
2015-09-02 17:13:54 +0900

    meta: Add result_or()
    result_or(v, f, args) calls f(args...) and returns v or the result of
    function call depending on whether the return type of the function is
    void or not. Sometimes useful in generic code.
    
    This commit also makes ext::n4502 namespace non-inline because otherwise
    ext::n4502::detail conflicts with ext::detail.

--- include/ext/meta.hpp
+++ include/ext/meta.hpp
@@ -2,6 +2,7 @@
 #define EXT_META_HPP
 
 #include <type_traits>
+#include <utility>
 
 
 namespace ext
@@ -16,9 +17,45 @@ namespace ext
     using make_non_deduced_t = typename make_non_deduced<T>::type;
 }
 
+namespace ext
+{
+    namespace detail
+    {
+        template<typename T, typename Function, typename... Args>
+        T result_or(std::true_type,
+                    T const& default_value,
+                    Function&& fun, Args&&... args)
+        {
+            std::forward<Function>(fun)(std::forward<Args>(args)...);
+            return default_value;
+        }
+
+        template<typename T, typename Function, typename... Args>
+        decltype(auto) result_or(std::false_type,
+                                 T const&,
+                                 Function&& fun, Args&&... args)
+        {
+            return std::forward<Function>(fun)(std::forward<Args>(args)...);
+        }
+    }
+
+    /*
+     * Returns fun(args) if it is not void or default_value if it is.
+     */
+    template<typename T, typename Function, typename... Args>
+    decltype(auto) result_or(T const& default_value, Function&& fun, Args&&... args)
+    {
+        using result_type = decltype(std::forward<Function>(fun)(std::forward<Args>(args)...));
+        return detail::result_or(std::is_same<result_type, void>(),
+                                 default_value,
+                                 std::forward<Function>(fun),
+                                 std::forward<Args>(args)...);
+    }
+}
+
 //___ N4502 detection idiom __________________________________________________
 
-namespace ext { inline namespace n4502
+namespace ext { namespace n4502
 {
     namespace detail
     {
@@ -74,5 +111,13 @@ namespace ext { inline namespace n4502
     template<template<typename> class Op, typename... Args>
     using detected_t = typename detail::detector<nonesuch, void , Op, Args...>::type;
 }}
+
+namespace ext
+{
+    using n4502::void_t;
+    using n4502::nonesuch;
+    using n4502::is_detected;
+    using n4502::detected_t;
+}
 
 #endif
