c5d93e99d3c350534d629e74b984dfbd047c8e6b
sinfu <nobody@example.com>
2016-05-16 06:45:54 +0900

    Add ext::value_ptr

--- include/ext/smart_pointer.hpp
+++ include/ext/smart_pointer.hpp
@@ -2,6 +2,8 @@
 #define EXT_SMART_POINTER_HPP
 
 #include <memory>
+#include <type_traits>
+#include <utility>
 
 namespace ext
 {
@@ -19,6 +21,166 @@ namespace ext
     {
         return std::shared_ptr<T>{std::shared_ptr<T>{}, ptr};
     }
+
+    /*
+     * Base class for polymorphically invoking copy constructor of an object
+     * of a subclass of type T.
+     */
+    template<typename T>
+    struct polymorphic_value
+    {
+        polymorphic_value() = default;
+        polymorphic_value(polymorphic_value const&) = delete;
+        polymorphic_value& operator=(polymorphic_value const&) = delete;
+
+        virtual ~polymorphic_value() noexcept = default;
+        virtual std::unique_ptr<polymorphic_value> copy() = 0;
+        virtual T& value() noexcept = 0;
+    };
+
+    /*
+     * Implementation of polymorphic_value<T> where the copy constructor
+     * of U is properly invoked via the copy() member function.
+     */
+    template<typename T, typename U>
+    struct concrete_value : polymorphic_value<T>
+    {
+        static_assert(std::is_convertible<U*, T*>::value, "U must be a subclass of T");
+
+        explicit
+        concrete_value(U const& instance)
+            : value_{instance}
+        {
+        }
+
+        explicit
+        concrete_value(U&& instance)
+            : value_{std::move(instance)}
+        {
+        }
+
+        std::unique_ptr<polymorphic_value<T>> copy() override
+        {
+            // Get around possible non-copy constructor U(U&)
+            return std::make_unique<concrete_value>(const_cast<U const&>(value_));
+        }
+
+        T& value() noexcept override
+        {
+            return value_;
+        }
+
+      private:
+        U value_;
+    };
+
+    /*
+     * Smart pointer with value semantics. Typical use would be for holding a
+     * pointer to base class for pimpl idiom.
+     */
+    template<typename T>
+    struct value_ptr
+    {
+        using element_type = T;
+        using value_type = std::remove_const_t<T>;
+        using pointer = T*;
+        using reference = T&;
+
+        /*
+         * Initialises itself to the null state.
+         */
+        value_ptr() noexcept = default;
+
+        /*
+         * Initialises itself to the null state.
+         */
+        value_ptr(std::nullptr_t) noexcept
+            : value_ptr{}
+        {
+        }
+
+        /*
+         * Constructs a value_ptr pointing to an object of given value.
+         */
+        template<typename U,
+                 std::enable_if_t<std::is_convertible<U*, T*>::value, int> = 0>
+        explicit
+        value_ptr(U value)
+            : store_{std::make_unique<concrete_value<T, U>>(std::move(value))}
+        {
+        }
+
+        /*
+         * Initialises the owned object to be a copy of the other one.
+         */
+        value_ptr(value_ptr const& other)
+            : store_{other.store_->copy()}
+        {
+        }
+
+        value_ptr& operator=(value_ptr const& other)
+        {
+            value_ptr{other}.swap(*this);
+            return *this;
+        }
+
+        /*
+         * Grabs the object owned by other value_ptr.
+         */
+        value_ptr(value_ptr&& other) noexcept = default;
+        value_ptr& operator=(value_ptr&& other) noexcept = default;
+
+        /*
+         * Swaps the underlying pointer with that of the other object.
+         */
+        void swap(value_ptr& other) noexcept
+        {
+            store_.swap(other.store_);
+        }
+
+        /*
+         * Destroys owned object, if any, and sets itself to the null state.
+         */
+        void reset() noexcept
+        {
+            store_.reset();
+        }
+
+        /*
+         * value_ptr always evaluates to true (non-null) as a pointer.
+         */
+        explicit operator bool() const noexcept
+        {
+            return static_cast<bool>(store_);
+        }
+
+        /*
+         * Returns a pointer to the stored object.
+         */
+        pointer get() const noexcept
+        {
+            return store_ ? &store_->value() : nullptr;
+        }
+
+        /*
+         * The usual arrow operator.
+         */
+        pointer operator->() const noexcept
+        {
+            return get();
+        }
+
+        /*
+         * Returns a reference to the stored object.
+         */
+        reference operator*() const noexcept
+        {
+            return *get();
+        }
+
+      private:
+        std::unique_ptr<polymorphic_value<T>> store_;
+    };
 }
 
 #endif
--- test/smart_pointer.cc
+++ test/smart_pointer.cc
@@ -3,6 +3,10 @@
 #include <memory>
 #include <ext/smart_pointer.hpp>
 
+//----------------------------------------------------------------------------
+// view_as_shared
+//----------------------------------------------------------------------------
+
 TEST(ViewAsShared, ViewLocal)
 {
     int var = 42;
@@ -12,4 +16,71 @@ TEST(ViewAsShared, ViewLocal)
         *ptr = 10;
     }
     ASSERT_EQ(var, 10);
+}
+
+//----------------------------------------------------------------------------
+// value_ptr
+//----------------------------------------------------------------------------
+
+TEST(ValuePtr, Null)
+{
+    ext::value_ptr<int> null1;
+    ASSERT_FALSE(null1);
+    ASSERT_EQ(null1.get(), nullptr);
+
+    ext::value_ptr<int> null2{nullptr};
+    ASSERT_FALSE(null2);
+    ASSERT_EQ(null2.get(), nullptr);
+}
+
+TEST(ValuePtr, Copy)
+{
+    ext::value_ptr<int> val{42};
+    ext::value_ptr<int> copy{val};
+    ASSERT_TRUE(val);
+    ASSERT_TRUE(copy);
+    ASSERT_EQ(*copy, 42);
+    *copy = 10;
+    ASSERT_EQ(*copy, 10);
+    ASSERT_EQ(*val, 42);
+}
+
+TEST(ValuePtr, Move)
+{
+    ext::value_ptr<int> val{42};
+    ext::value_ptr<int> move{std::move(val)};
+    ASSERT_FALSE(val);
+    ASSERT_TRUE(move);
+    ASSERT_EQ(*move, 42);
+}
+
+TEST(ValuePtr, Polymorphism)
+{
+    struct base
+    {
+        virtual ~base() = default;
+        virtual int value() = 0;
+    };
+    struct derived : base
+    {
+        int value_;
+        explicit derived(int val) : value_{val} {}
+        int value() override { return value_; }
+    };
+
+    ext::value_ptr<base> foo{derived{42}};
+    ASSERT_TRUE(foo);
+    ASSERT_EQ(foo->value(), 42);
+
+    // Copy without slicing
+    ext::value_ptr<base> foo_copy{foo};
+    ASSERT_EQ(foo_copy->value(), 42);
+
+    // Assign new value without slicing
+    foo = ext::value_ptr<base>{derived{10}};
+    ASSERT_EQ(foo->value(), 10);
+
+    // Move without slicing
+    ext::value_ptr<base> foo_move{std::move(foo)};
+    ASSERT_EQ(foo_move->value(), 10);
 }
